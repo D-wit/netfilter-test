d6d4cf29b0e713097efb740ce555ca0c6a15d5a1
Daegi Kim <86525755+D-wit@users.noreply.github.com>
2021-08-24 10:52:32 +0900

    Update iptables.c
    remove UDP Drop rule

--- iptables.c
+++ iptables.c
@@ -6,6 +6,4 @@ void iptables()
     system("iptables -F");
     system("iptables -A OUTPUT -j NFQUEUE --queue-num 0");
     system("iptables -A INPUT -j NFQUEUE --queue-num 0");
-    system("iptables -A OUTPUT -p udp -j DROP");
-    system("iptables -A INPUT -p udp -j DROP");
 }
